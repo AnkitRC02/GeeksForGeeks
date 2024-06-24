24bc78c5dc495afeb61a84dec694b056488fa63a
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-06-24 18:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def sumMatrix(self, n, q):
+        start = max(1, q - n)
+        end = min(n, q - 1)
+        if start <= end:
+            return end - start + 1
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n, q = map(int, input().split())
+
+        ob = Solution()
+        print(ob.sumMatrix(n, q))
+
+# } Driver Code Ends
