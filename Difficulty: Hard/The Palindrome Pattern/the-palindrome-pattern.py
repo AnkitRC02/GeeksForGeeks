54abc81b5b73983133e1c1fdc5c6f03ea3af57a3
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-06-28 20:40:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Palindrome Pattern/the-palindrome-pattern.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    def pattern(self, arr):
+        n = len(arr)
+        
+        def is_palindrome(seq):
+            return seq == seq[::-1]
+        
+        for i in range(n):
+            if is_palindrome(arr[i]):
+                return "{} R".format(i)
+        
+        for j in range(n):
+            col = [arr[i][j] for i in range(n)]
+            if is_palindrome(col):
+                return "{} C".format(j)
+        
+        return "-1"
+
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    ob = Solution()
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        a = list()
+        c = 0
+        for i in range(0, n):
+            X = list()
+            for j in range(0, n):
+                X.append(arr[c])
+                c += 1
+            a.append(X)
+        ans = ob.pattern(a)
+        print(ans)
+
+# } Driver Code Ends
