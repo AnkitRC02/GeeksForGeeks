67834ae4915ba44f68162e2fd1e99f7afce0a116
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-07-31 20:07:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def longestCommonPrefix(self, arr):
+        if not arr:
+            return "-1"
+        
+        prefix = arr[0]
+        for i in range(1, len(arr)):
+            while arr[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return "-1"
+        
+        return prefix if prefix else "-1"
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for _ in range(t):
+        arr = [x for x in input().strip().split(" ")]
+
+        ob = Solution()
+        print(ob.longestCommonPrefix(arr))
+
+# } Driver Code Ends
