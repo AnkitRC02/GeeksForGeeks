c1679fe2e29db88fcb63ea4cf4e083a05615a653
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-10-15 10:08:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.py
@@ -0,0 +1,41 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+from itertools import accumulate
+
+from collections import defaultdict
+class Solution:
+
+    def subArraySum(self,arr, tar):
+        
+        count = 0
+        dc = defaultdict(int)
+        dc[0]=1
+        for presum in accumulate(arr):
+            count += dc[presum-tar]
+            dc[presum]+=1
+        return count
+        
+        
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        tar= int(input())
+        ob = Solution()
+        res = ob.subArraySum(arr,tar)
+        print(res)
+        # print("~")
+        t -= 1
+
+
+# } Driver Code Ends
