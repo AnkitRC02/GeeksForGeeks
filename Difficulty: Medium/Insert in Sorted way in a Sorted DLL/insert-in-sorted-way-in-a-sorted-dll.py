df8da6eb398daafd2aedaa0b5b2f8736443ed13f
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-10-31 10:48:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert in Sorted way in a Sorted DLL/insert-in-sorted-way-in-a-sorted-dll.py
@@ -0,0 +1,69 @@
+#User function Template for python3
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+'''
+class Solution:
+    #Function to insert a node in a sorted doubly linked list.
+    def sortedInsert(self, head, x):
+    #code here
+        new=Node(x)
+        if x<=head.data:
+            new.next=head
+            return new
+        temp=head
+        while temp.next:
+            if x>=temp.data and x<=temp.next.data:
+                new.next=temp.next
+                temp.next=new
+                return  head
+            temp=temp.next
+        temp.next=new
+        return head
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+def print_list(head):
+    current = head
+    while current:
+        print(current.data, end=' ')
+        current = current.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        values = list(map(int, input().strip().split()))
+        head = None
+        tail = None
+
+        for value in values:
+            if head is None:
+                head = Node(value)
+                tail = head
+            else:
+                tail.next = Node(value)
+                tail.next.prev = tail
+                tail = tail.next
+
+        value_to_insert = int(input().strip())
+        solution = Solution()
+        head = solution.sortedInsert(head, value_to_insert)
+        print_list(head)
+
+        print("~")
+
+# } Driver Code Ends
