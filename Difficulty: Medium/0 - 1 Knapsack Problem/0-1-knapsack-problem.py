d572136a3d07b916c99aa4d3c748a352e53941e4
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2025-03-13 09:15:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -1,36 +1,30 @@
 #User function Template for python3
 
 class Solution:
-    def knapSack(self, W, wt, val):
-        N = len(val)
-        dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
-        
-        for i in range(1, N + 1):
-            for w in range(1, W + 1):
-                if wt[i - 1] <= w:
-                    dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
-                else:
-                    dp[i][w] = dp[i - 1][w]
-                    
-        return dp[N][W]
+    # Function to return max value that can be put in knapsack of capacity.
+    def knapsack(self, W, val, wt):
+        # code here
+        n = len(wt)
+        dp = [[0]*(n+1) for _ in range(W+1)]
+        # dp[i][j] = max(dp[i][j], dp[i-wt[j]][j-1]+)
+        for w in range(1, W+1):
+            for i in range(n):
+                dp[w][i+1] = dp[w][i]
+                if wt[i] <= w:
+                    dp[w][i+1] = max(dp[w][i+1], dp[w-wt[i]][i]+val[i])
+        return dp[-1][-1]
 
 
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
-import atexit
-import io
-import sys
-
 if __name__ == '__main__':
     test_cases = int(input())
-    for cases in range(test_cases):
-        # n = int(input())
-        W = int(input())
-        val = list(map(int, input().strip().split()))
-        wt = list(map(int, input().strip().split()))
+    for _ in range(test_cases):
+        capacity = int(input())
+        values = list(map(int, input().strip().split()))
+        weights = list(map(int, input().strip().split()))
         ob = Solution()
-        print(ob.knapSack(W, wt, val))
-
+        print(ob.knapsack(capacity, values, weights))
+        print("~")
 # } Driver Code Ends
